9170cb1a7131d35081873436ab0c85107330e233
Александр Владимирович Высоцкий <79597004+89131587376@users.noreply.github.com>
2021-04-05 11:15:28 +0600

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int rrand(int range_min, int range_max) {
+    return rand() % (range_max - range_min + 1) + range_min;
+
+}
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+
+    srand(static_cast<unsigned int>(time(NULL)));
+
+
+    const unsigned int ARRSIZE = 10;
+    const int ABSLIMIT = 100;
+
+    int ary[ARRSIZE];
+
+        
+    for (unsigned int i = 0; i < ARRSIZE; i++) {
+            ary[i] = rrand(-ABSLIMIT, ABSLIMIT);
+        }
+
+    for (int i = 0; i < ARRSIZE; i++) {
+        cout << ary[i] << endl;
+    }
+    int min = 0;
+
+    for (int i = 0; i < ARRSIZE; i++)
+    {
+        if (ary[min] > ary[i]) min = i;
+    }
+    cout << "\n минимальный элемент в массиве[" << min << "] = " << ary[min];
+}
