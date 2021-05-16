e851c0c3f47be829f95c65c158c4f45ec33678a4
Александр Владимирович Высоцкий <79597004+89131587376@users.noreply.github.com>
2021-05-16 09:36:09 +0600

    Update lab5_7.cpp

--- lab5_7.cpp
+++ lab5_7.cpp
@@ -4,30 +4,30 @@
  Задание№: 5.7
  Цель:написать программу кодировки (раскадировки) текста
 */
-#define _CRT_SECURE_NO_WARNINGS 
-#include <iostream> 
-#include <string> 
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
  
-использование пространства имен std; 
-char* str_input(int * str_len); 
-char* encode(char* str, int* str_len, int bias); 
-char* decode(char* str, int* str_len, int bias); 
-int main() { 
- int* str_len = new int; 
- инт предвзятость; 
- cout <: "; 
- cin >>> смещение; 
- cin.ignore(32767, '\n'); 
- char* str = str_input(str_len); 
- char* encoded_str = encode(str, str_len, bias); 
- char* decoded_str = decode(encoded_str, str_len, bias); 
- cout << "Default str:" << str <; 
- cout << "Encoded str:" << encoded_str <; 
- cout << "Decoded str:" << decoded_str <; 
+using namespace std;
+char* str_input(int * str_len);
+char* encode(char* str, int* str_len, int bias);
+char* decode(char* str, int* str_len, int bias);
+int main() {
+	int* str_len = new int;
+	int bias;
+	cout << "Enter bias: ";
+	cin >> bias;
+	cin.ignore(32767, '\n');
+	char* str = str_input(str_len);
+	char* encoded_str = encode(str, str_len, bias);
+	char* decoded_str = decode(encoded_str, str_len, bias);
+	cout << "Default str: " << str << endl;
+	cout << "Encoded str: " << encoded_str << endl;
+	cout << "Decoded str: " << decoded_str << endl;
 }
-char* str_input(int* str_len) 
+char* str_input(int* str_len)
 {
- строка строка; 
+	string string;
 	cout << "Enter string: ";
 	getline(cin, string);
 	*str_len = string.length();
@@ -40,19 +40,19 @@ char* str_input(int* str_len)
 char* encode(char* str, int* str_len, int bias)
 {
 	char* new_str = new char[*str_len];
- strcpy(new_str, str); 
- for (int i = 0; i) { 
- new_str[i] = new_str[i] + смещение; 
+	strcpy(new_str, str);
+	for (int i = 0; i < *str_len; i++) {
+		new_str[i] = new_str[i] + bias;
 	}
- return new_str; 
+	return new_str;
 }
  
-char* decode(char* str, int* str_len, int bias) 
+char* decode(char* str, int* str_len, int bias)
 {
- char* new_str = new char[*str_len]; 
- strcpy(new_str, str); 
- for (int i = 0; i) { 
- new_str[i] = new_str[i] - смещение; 
+	char* new_str = new char[*str_len];
+	strcpy(new_str, str);
+	for (int i = 0; i < *str_len; i++) {
+		new_str[i] = new_str[i] - bias;
 	}
- return new_str; 
+	return new_str;
 }
